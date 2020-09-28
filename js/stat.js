09283f042b4fd47aa737f43f78be3915a9ab26cc
ilya <65188527+ilyaip@users.noreply.github.com>
2020-09-28 20:31:56 +0300

    Training project: canvas

--- js/stat.js
+++ js/stat.js
@@ -8,18 +8,25 @@ const GAP = 10;
 const FONT_GAP = 250;
 const BAR_GAP = 50;
 const BAR_WIDTH = 40;
-const barHeight = 150;
+const BAR_HEIGHT = 150;
+const CLOUD_COLOR = "rgba(0, 0, 0, 0.7)";
+const CLOUD_COLOR_BACK = "#fff";
+const TEXT_COLOR = "#000";
+const TEXT_FONT = "16px PT Mono";
+const TEXT_RED = "rgba(255, 0, 0, 1)";
+const MAIN_PLAYER = "Вы";
+const LEFT_MARGIN = 40;
 
 const renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
-  for (var i = 1; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
+var getMaxElement = function (array) {
+  var maxElement = array[0];
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] > maxElement) {
+      maxElement = array[i];
     }
   }
   return maxElement;
@@ -30,11 +37,11 @@ function getRandomInt(max) {
 }
 
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, "rgba(0, 0, 0, 0.7)");
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, "#fff");
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR_BACK);
 
-  ctx.fillStyle = "#000";
-  ctx.font = "16px PT Mono";
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.font = TEXT_FONT;
   ctx.fillText("Ура вы победили!", 140, 40);
   ctx.fillText("Список результатов:", 140, 60);
 
@@ -42,14 +49,14 @@ window.renderStatistics = function (ctx, players, times) {
 
   for (var i = 0; i < players.length; i++) {
     var randomColor = getRandomInt(100);
-    if (players[i] === "Вы") {
-      ctx.fillStyle = "rgba(255, 0, 0, 1)";
+    if (players[i] === MAIN_PLAYER) {
+      ctx.fillStyle = TEXT_RED;
     } else {
       ctx.fillStyle = `hsl(240, ${randomColor}%, 30%)`;
     }
-    ctx.fillRect(CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * i, FONT_GAP - GAP, BAR_WIDTH, -(barHeight * times[i]) / maxTime);
-    ctx.fillStyle = "#000";
-    ctx.fillText(players[i], CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + FONT_GAP);
-    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - (barHeight * times[i]) / maxTime - BAR_WIDTH);
+    ctx.fillRect(CLOUD_X + LEFT_MARGIN + (BAR_WIDTH + BAR_GAP) * i, FONT_GAP - GAP, BAR_WIDTH, - (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillStyle = TEXT_FONT;
+    ctx.fillText(players[i], LEFT_MARGIN + CLOUD_X + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + FONT_GAP);
+    ctx.fillText(Math.round(times[i]), LEFT_MARGIN + CLOUD_X + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime - BAR_WIDTH);
   }
 };
