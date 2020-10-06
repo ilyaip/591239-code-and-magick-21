708e3ab5e2a4156eb986b7285bfefb3e044157f7
ilya <65188527+ilyaip@users.noreply.github.com>
2020-10-06 14:00:48 +0300

    Training project: we are the Horde

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,8 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+var NUMBER_OF_WIZARD = 4;
+
 var userDialog = document.querySelector(".setup");
 userDialog.classList.remove("hidden");
 
@@ -22,32 +24,24 @@ var COAT_COLORS = ["rgb(101, 137, 164)", "rgb(241, 43, 107)", "rgb(146, 100, 161
 
 var EYES_COLORS = ["black", "red", "blue", "yellow", "green"];
 
-var wizards = [
-  {
+var getWizard = function () {
+  return {
     name: WIZARD_NAMES[getRandomInt(0, WIZARD_NAMES.length - 1)],
     surname: WIZARD_SURNAMES[getRandomInt(0, WIZARD_SURNAMES.length - 1)],
     coatColor: COAT_COLORS[getRandomInt(0, COAT_COLORS.length - 1)],
     eyesColor: EYES_COLORS[getRandomInt(0, EYES_COLORS.length - 1)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomInt(0, WIZARD_NAMES.length - 1)],
-    surname: WIZARD_SURNAMES[getRandomInt(0, WIZARD_SURNAMES.length - 1)],
-    coatColor: COAT_COLORS[getRandomInt(0, COAT_COLORS.length - 1)],
-    eyesColor: EYES_COLORS[getRandomInt(0, EYES_COLORS.length - 1)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomInt(0, WIZARD_NAMES.length - 1)],
-    surname: WIZARD_SURNAMES[getRandomInt(0, WIZARD_SURNAMES.length - 1)],
-    coatColor: COAT_COLORS[getRandomInt(0, COAT_COLORS.length - 1)],
-    eyesColor: EYES_COLORS[getRandomInt(0, EYES_COLORS.length - 1)]
-  },
-  {
-    name: WIZARD_NAMES[getRandomInt(0, WIZARD_NAMES.length - 1)],
-    surname: WIZARD_SURNAMES[getRandomInt(0, WIZARD_SURNAMES.length - 1)],
-    coatColor: COAT_COLORS[getRandomInt(0, COAT_COLORS.length - 1)],
-    eyesColor: EYES_COLORS[getRandomInt(0, EYES_COLORS.length - 1)]
+  };
+};
+
+var getWizards = function (number) {
+  var wizards = [];
+  for (var i = 0; i < number; i++) {
+    wizards[i] = getWizard();
   }
-];
+  return wizards;
+};
+
+var wizards = getWizards(NUMBER_OF_WIZARD);
 
 for (var i = 0; i < WIZARD_NAMES.length; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
@@ -55,6 +49,6 @@ for (var i = 0; i < WIZARD_NAMES.length; i++) {
   wizardElement.querySelector(".setup-similar-label").textContent = wizards[i].name + " " + wizards[i].surname;
   wizardElement.querySelector(".wizard-coat").style.fill = wizards[i].coatColor;
   wizardElement.querySelector(".wizard-eyes").style.fill = wizards[i].eyesColor;
-  
+
   similarListElement.appendChild(wizardElement);
-}
+};
